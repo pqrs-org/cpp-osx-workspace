e28178206f3ae6381049ca70e4efe2e74405f6e3
Takayama Fumihiko <tekezo@pqrs.org>
2022-12-24 11:27:49 +0900

    Use std::string.c_str() for bundle_identifier instead of std::string_view.data() which is not guaranteed to be null terminated

--- include/pqrs/osx/workspace.hpp
+++ include/pqrs/osx/workspace.hpp
@@ -7,14 +7,14 @@
 // (See http://www.boost.org/LICENSE_1_0.txt)
 
 #include "workspace/impl/objc.h"
-#include <string_view>
+#include <string>
 
 namespace pqrs {
 namespace osx {
 namespace workspace {
 
-inline std::string find_application_url_by_bundle_identifier(const std::string_view& bundle_identifier) {
-  auto url = pqrs_osx_workspace_find_application_url_by_bundle_identifier(bundle_identifier.data());
+inline std::string find_application_url_by_bundle_identifier(const std::string& bundle_identifier) {
+  auto url = pqrs_osx_workspace_find_application_url_by_bundle_identifier(bundle_identifier.c_str());
   if (url == nullptr) {
     return "";
   }
