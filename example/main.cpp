c883343cc64a18819ad87a31a942f2ca8f4e2651
Takayama Fumihiko <tekezo@pqrs.org>
2025-05-28 08:31:14 +0900

    Add open_configuration

--- example/main.cpp
+++ example/main.cpp
@@ -15,8 +15,17 @@ int main(void) {
   std::cout << "NotFound: "
             << pqrs::osx::workspace::find_application_url_by_bundle_identifier("org.pqrs.NotFound") << std::endl;
 
-  pqrs::osx::workspace::open_application_by_bundle_identifier("com.apple.Safari");
+  pqrs::osx::workspace::open_application_by_bundle_identifier("com.apple.Safari",
+                                                              pqrs::osx::workspace::open_configuration{
+                                                                  .activates = false,
+                                                              });
   pqrs::osx::workspace::open_application_by_file_path("/System/Applications/Utilities/Activity Monitor.app");
+
+  // pqrs::osx::workspace::open_application_by_file_path(
+  //     "/Library/Application Support/org.pqrs/Karabiner-Elements/Karabiner-Updater.app",
+  //     pqrs::osx::workspace::open_configuration{
+  //         .arguments = {"checkForUpdatesWithBetaVersion"},
+  //     });
 
   std::cout << pqrs::osx::workspace::application_running_by_bundle_identifier("com.apple.Safari") << std::endl;
   std::cout << pqrs::osx::workspace::application_running_by_file_path("/System/Applications/Utilities/Activity Monitor.app") << std::endl;
